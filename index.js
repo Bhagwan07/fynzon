934d76f8598cc4d1e7d41666ea0d551df0042ccc
Bhagwan Navthar <virtualgaintechnologies007@gmail.com>
2024-02-12 09:51:46 +0530

    .

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const dns = require("dns");
+
+app.get("/api/v1/testIp", (req, res, next) => {
+  try {
+    const hostname = req.hostname;
+    // const hostname = "testapi.fynzon.com";
+    let ip;
+    dns.resolve(hostname, (err, addresses) => {
+      if (err) {
+        console.error(`Failed to resolve ${hostname}: ${err}`);
+        return;
+      }
+
+      console.log(`IP addresses for ${hostname}:`);
+      addresses.forEach((address, index) => {
+        console.log(`- ${address}`);
+      });
+      ip = addresses;
+      res.send(addresses);
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Express running on port 3000");
+});
