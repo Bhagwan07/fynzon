836b11527639746ff4711b085b3e7d6f8235a0ee
Bhagwan Navthar <virtualgaintechnologies007@gmail.com>
2024-02-15 16:17:58 +0530

    .

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const app = express();
-const dns = require("dns");
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
+const expressip = require("express-ip");
+app.use(expressip().getIpInfoMiddleware);
+
 app.get("/api/v1/testIp", (req, res, next) => {
   try {
-    const ipreq = req.ip;
-    const clientIP =
-      req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-    console.log(clientIP);
-    res.send({ clientIP, ipreq });
+    res.send(req.ipInfo);
   } catch (err) {
     next(err);
   }
